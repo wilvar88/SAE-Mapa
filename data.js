6b1c91d126fa7fe197b79243f92eae8270872eb1
wilvar88 <138652332+wilvar88@users.noreply.github.com>
2025-08-13 04:01:11 -0500

    Update data.js

--- data.js
+++ data.js
@@ -1,18 +1,21 @@
 // data.js — puntos de ejemplo. Edita/añade los tuyos.
 // x,y en PORCENTAJE (0–100) respecto a la imagen.
 window.HOTSPOTS = [
-  { x: 10.0, y: 7.5, n: 1,  title: "Buscar direcciones",        text: ""},
-  { x: 68.0, y: 7.5, n: 2,  title: "Modo de vista",             text: "Estandar o Satelital"},
-  { x: 80.0, y: 7.5, n: 3,  title: "Buscador",                  text: "" },
-  { x: 83.0, y: 7.5, n: 4,  title: "Medir distancia",           text: "" },
-  { x: 85.7, y: 7.5, n: 5,  title: "Mostrar Convenciones",      text: "" },
-  { x: 88.7, y: 7.5, n: 6,  title: "Ocultar Paradas",           text: "" },
-  { x: 91.5, y: 7.5, n: 7,  title: "Ocultar Líneas",            text: "" },
-  { x: 94.2, y: 7.5, n: 8,  title: "Filtrado",                  text: "" },
-  { x: 90.9, y: 20.0, n: 9,  title: "Zoom in",                  text: "" },
-  { x: 92.8, y: 20.0, n: 10, title: "Zoom Out",                 text: "" },
-  { x: 94.8, y: 20.0, n: 11, title: "Reset",                    text: "" },
-  { x: 58.5, y: 37.2, n: 12, title: "Paraderos",                text: "" },
-  { x: 57.1, y: 59.8, n: 13, title: "Trazado de la ruta",       text: "" },
+  { x: 6.8, y: 8.3, n: 1,  title: "Buscar direcciones",         text: ""},
+  { x: 58.0, y: 8.3, n: 2,  title: "Modo de vista",             text: "Estandar o Satelital"},
+  { x: 69.5, y: 8.3, n: 3,  title: "Buscador",                  text: "Al tener alguna línea seleccionada con su trayectoria y parada. Esta opción nos permite buscar paradas o vehículos específicos y nos lo centra en el mapa." },
+  { x: 72.0, y: 8.3, n: 4,  title: "Medir distancia",           text: "Sirve para marcar puntos en el mapa y nos traiga el total de la distancia medida de todos los puntos seleccionados." },
+  { x: 74.6, y: 8.3, n: 5,  title: "Mostrar Convenciones",      text: "Esta opción muestra u oculta al usuario una ventana emergente que detalla la convención utilizada. Mostrando una descripción de los símbolos y colores utilizados en el mapa para representar elementos como los vehículos, colores de regulación y velocidad." },
+  { x: 77.1, y: 8.3, n: 6,  title: "Mostrar u Ocultar Paradas", text: "Permite al usuario mostrar u ocultar temporalmente los marcadores que representan las paradas en el mapa; Al hacer clic los iconos de las paradas desaparecerán de la vista del mapa. Esto es útil al momento de simplificar la visualización." },
+  { x: 79.7, y: 8.3, n: 7,  title: "Mostrar u Ocultar Líneas",  text: "Permite al usuario mostrar u ocultar temporalmente las representaciones visuales de las rutas y trayectos del mapa. Al activar esta opción las líneas que conectan las paradas y los vehículos se vuelven invisibles dando una visualización mas limpia." },
+  { x: 82.2, y: 8.3, n: 8,  title: "Filtrado",                  text: "Permite al usuario seleccionar: mostrar vehículos sin servicio, mostrar patios, operadores y trayectos específicos de interés en el mapa. El usuario puede seleccionar que rutas y trayectos desea ver en la vista del mapa. Esto es importante para realizar seguimiento detallado a rutas especificas y realizar un buen seguimiento." },
+  { x: 79.1, y: 19.8, n: 9,  title: "Zoom in",                  text: "Permite al usuario aumentar la escala del mapa." },
+  { x: 81.0, y: 19.8, n: 10, title: "Zoom Out",                 text: "Permite al usuario disminuir la escala del mapa." },
+  { x: 82.9, y: 19.8, n: 11, title: "Reset",                    text: "" },
+  { x: 50.6, y: 34.4, n: 12, title: "Paraderos",                text: "" },
+  { x: 49.4, y: 54.0, n: 13, title: "Trazado de la ruta",       text: "" },
+  { x: 92.2, y: 40.8, n: 14, title: "Vehículo en Movimiento",      text: "Cuando el vehículo esta en movimiento se representa como un triangulo equilátero con una pequeña barra en su parte inferior. La parte superior del triangulo que forma la punta, representa la parte delantera del vehículo y la barra inferior representa la regulación." },
+  { x: 92.2, y: 63.5, n: 15, title: "Vehículo en reposo",      text: "cuando el vehículo esta en reposo o detenido; el icono cambia a un circulo, pero sigue mostrando la barra que representa su estado de regulación." },
 ];
+
 
